77a60f3359590cdb4c24817ef1a3514ad524a445
emma t <emma@scape.io>
2019-07-04 18:00:06 +0100

    migrate to androidx and use local scapekit 1.0.2

--- pixscape/app/src/main/java/com/scape/pixscape/PixscapeApp.kt
+++ pixscape/app/src/main/java/com/scape/pixscape/PixscapeApp.kt
@@ -2,13 +2,16 @@ package com.scape.pixscape
 
 import android.app.Application
 import android.util.Log
+import com.scape.scapekit.LogLevel
+import com.scape.scapekit.LogOutput
 import com.scape.scapekit.Scape
 import com.scape.scapekit.ScapeClient
+import java.util.*
 
 /**
  * Basic Application class that demonstrates the initialisation of the ScapeClient.
  *
- * ScapeClient entry point is acquired with debugSupport and ArSupport enabled and with our own Api Key.
+ * ScapeClient entry point is acquired with ArSupport enabled and with our own Api Key.
  */
 class PixscapeApp : Application() {
 
@@ -34,8 +37,11 @@ class PixscapeApp : Application() {
 
         scapeClient = Scape.scapeClientBuilder
                 .withContext(applicationContext)
-                .withDebugSupport(false)
+                .withDebugSupport(true)
                 .withApiKey(BuildConfig.SCAPEKIT_API_KEY)
                 .build()
+
+        scapeClient.debugSession?.setLogConfig(LogLevel.LOG_DEBUG, EnumSet.of(LogOutput.CONSOLE))
+        scapeClient.debugSession?.mockGPSCoordinates(51.553049, -0.124107)
     }
 }
