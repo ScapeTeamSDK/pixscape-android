4815e95695680f59126b3c04e8079beaeacb74b8
emma t <emma@scape.io>
2019-09-24 17:30:38 +0100

    forward more error messages to the user

--- pixscape-app/app/src/main/java/com/scape/pixscape/manager/ScapeClientManager.kt
+++ pixscape-app/app/src/main/java/com/scape/pixscape/manager/ScapeClientManager.kt
@@ -36,6 +36,7 @@ class ScapeClientManager private constructor(context: Context) {
 
     private var gpsLocations: MutableList<RouteSection> = ArrayList()
     private var scapeLocations: MutableList<RouteSection> = ArrayList()
+    private var scapeMeasurementsStatus: ScapeMeasurementsStatus = ScapeMeasurementsStatus.RESULTS_FOUND
 
     init {
         this.context = context
@@ -60,68 +61,27 @@ class ScapeClientManager private constructor(context: Context) {
             override fun onScapeMeasurementsUpdated(scapeSession: ScapeSession?, measurements: ScapeMeasurements?) {
                 val scapeMeasurements = measurements ?: return
 
-                if (scapeMeasurements.measurementsStatus != ScapeMeasurementsStatus.RESULTS_FOUND) {
-                    return
+                when(scapeMeasurements.measurementsStatus) {
+                    ScapeMeasurementsStatus.NO_RESULTS -> {}
+                    ScapeMeasurementsStatus.UNAVAILABLE_AREA ->{
+                        scapeMeasurementsStatus = ScapeMeasurementsStatus.UNAVAILABLE_AREA
+                    }
+                    ScapeMeasurementsStatus.RESULTS_FOUND -> {
+                        broadcastScapeMeasurements(scapeMeasurements)
+                    }
+                    ScapeMeasurementsStatus.INTERNAL_ERROR -> {}
                 }
-
-                val score = scapeMeasurements.confidenceScore ?: 0.0
-                if(score < 3.0) {
-                    return
-                }
-
-                val newLatLng = scapeMeasurements.latLng
-                if(newLatLng != null) {
-                    // single mode is enabled so the RouteSection's beginning and end will be the same
-                    // basically a single location
-                    scapeLocations.add(RouteSection(com.scape.pixscape.models.dto.Location(newLatLng.latitude,
-                        newLatLng.longitude),
-                        com.scape.pixscape.models.dto.Location(newLatLng.latitude,
-                            newLatLng.longitude)))
-                }
-
-                val bundle = Bundle().apply {
-                    putParcelableArrayList(TrackTraceService.ROUTE_SCAPE_SECTIONS_DATA_KEY, scapeLocations as ArrayList<out Parcelable>)
-                }
-                val intent = Intent().apply {
-                    action = CameraFragment.BROADCAST_ACTION_SCAPE_LOCATION
-                    putExtras(bundle)
-                }
-
-                context?.sendBroadcast(intent)
             }
 
             override fun onDeviceLocationMeasurementsUpdated(session: ScapeSession?, measurements: LocationMeasurements?) {
                 if (measurements == null) return
 
-                val newLatLng = measurements.latLng
-                if(newLatLng != null) {
-                    // single mode is enabled so the RouteSection's beginning and end will be the same
-                    // basically a single location
-                    gpsLocations.add(RouteSection(com.scape.pixscape.models.dto.Location(newLatLng.latitude,
-                        newLatLng.longitude),
-                        com.scape.pixscape.models.dto.Location(newLatLng.latitude,
-                            newLatLng.longitude)))
-                }
-
-                val bundle = Bundle().apply {
-                    putParcelableArrayList(TrackTraceService.ROUTE_GPS_SECTIONS_DATA_KEY, gpsLocations as ArrayList<out Parcelable>)
-                }
-
-                val intent = Intent().apply {
-                    action = CameraFragment.BROADCAST_ACTION_GPS_LOCATION
-                    putExtras(bundle)
-                }
-
-                context?.sendBroadcast(intent)
+                broadcastLocationMeasurements(measurements)
             }
 
             override fun onScapeSessionError(session: ScapeSession?, state: ScapeSessionState, error: String) {
-                val intent = Intent().apply {
-                    action = CameraFragment.BROADCAST_ACTION_STOP_TIMER
-                    putExtra(TrackTraceService.SCAPE_ERROR_STATE_KEY, state.ordinal)
-                }
 
-                context?.sendBroadcast(intent)
+                broadcastScapeSessionState(state)
             }
 
             override fun onScapeMeasurementsRequested(session: ScapeSession?, timestamp: Double) {
@@ -130,4 +90,66 @@ class ScapeClientManager private constructor(context: Context) {
         })
     }
 
+    private fun broadcastScapeSessionState(state: ScapeSessionState) {
+        val intent = Intent().apply {
+            action = CameraFragment.BROADCAST_ACTION_STOP_TIMER
+            putExtra(TrackTraceService.SCAPE_ERROR_STATE_KEY, state.ordinal)
+
+            if (scapeMeasurementsStatus == ScapeMeasurementsStatus.UNAVAILABLE_AREA) {
+                putExtra(TrackTraceService.SCAPE_MEASUREMENTS_STATUS_KEY, scapeMeasurementsStatus.ordinal)
+            }
+        }
+
+        context?.sendBroadcast(intent)
+    }
+
+    private fun broadcastLocationMeasurements(measurements: LocationMeasurements) {
+        val newLatLng = measurements.latLng
+        if(newLatLng != null) {
+            // single mode is enabled so the RouteSection's beginning and end will be the same
+            // basically a single location
+            gpsLocations.add(RouteSection(com.scape.pixscape.models.dto.Location(newLatLng.latitude,
+                newLatLng.longitude),
+                com.scape.pixscape.models.dto.Location(newLatLng.latitude,
+                    newLatLng.longitude)))
+        }
+
+        val bundle = Bundle().apply {
+            putParcelableArrayList(TrackTraceService.ROUTE_GPS_SECTIONS_DATA_KEY, gpsLocations as ArrayList<out Parcelable>)
+        }
+
+        val intent = Intent().apply {
+            action = CameraFragment.BROADCAST_ACTION_GPS_LOCATION
+            putExtras(bundle)
+        }
+
+        context?.sendBroadcast(intent)
+    }
+
+    private fun broadcastScapeMeasurements(scapeMeasurements: ScapeMeasurements) {
+        val score = scapeMeasurements.confidenceScore ?: 0.0
+        if(score < 3.0) {
+            return
+        }
+
+        val newLatLng = scapeMeasurements.latLng
+        if(newLatLng != null) {
+            // single mode is enabled so the RouteSection's beginning and end will be the same
+            // basically a single location
+            scapeLocations.add(RouteSection(com.scape.pixscape.models.dto.Location(newLatLng.latitude,
+                newLatLng.longitude),
+                com.scape.pixscape.models.dto.Location(newLatLng.latitude,
+                    newLatLng.longitude)))
+        }
+
+        val bundle = Bundle().apply {
+            putParcelableArrayList(TrackTraceService.ROUTE_SCAPE_SECTIONS_DATA_KEY, scapeLocations as ArrayList<out Parcelable>)
+        }
+        val intent = Intent().apply {
+            action = CameraFragment.BROADCAST_ACTION_SCAPE_LOCATION
+            putExtras(bundle)
+        }
+
+        context?.sendBroadcast(intent)
+    }
 }
